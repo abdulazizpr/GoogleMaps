9977497202ca4c15d41dfd2d22efef56da9e81d7
abdulazizpr <abdulazizpriatna@gmail.com>
2016-11-12 22:07:22 +0700

    first commit

---
+++ app/src/main/java/com/example/abdulazizpriatna/googlemaps/MapsActivity.java
@@ -0,0 +1,293 @@
+package com.example.abdulazizpriatna.googlemaps;
+
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.renderscript.Double2;
+import android.support.annotation.FloatRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import android.Manifest;
+import android.support.v7.app.AlertDialog;
+import android.widget.TextView;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private static final int MY_PERMISSIONS_REQUEST = 99;//int bebas, maks 1 byte
+    private GoogleMap mMap;
+    private Marker mPosSekarang;
+    GoogleApiClient mGoogleApiClient ;
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
+
+    //untuk rumah makan
+    private GoogleMap wdMap;
+    private GoogleMap PadangOmuda;
+    private GoogleMap mKontrakan;
+
+    private LatLng Wadoel;
+    private LatLng PadangO;
+    private LatLng Kontrakan;
+    private LatLng posSekarang;
+
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    private void ambilLokasi() {
+       /* mulai Android 6 (API 23), pemberian persmission
+        dilakukan secara dinamik (tdk diawal)
+        untuk jenis2 persmisson tertentu, termasuk lokasi
+        */
+
+
+        // cek apakah sudah diijinkan oleh user, jika belum tampilkan dialog
+        if (ActivityCompat.checkSelfPermission (this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED )
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST);
+            return;
+        }
+        //set agar setiap update lokasi maka UI bisa diupdate
+        //setiap ada update maka onLocationChanged akan dipanggil
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                mLocationRequest, this);
+    }
+
+    protected  void  createLocationRequest(){
+        mLocationRequest = new LocationRequest();
+        //10 detik sekli minta lokasi (10000ms = 10 detik)
+        mLocationRequest.setInterval(10000);
+        //tapi tidak boleh cepat dari 5 detik
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        buildGoogleApiClient();
+        createLocationRequest();
+    }
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //untuk tempat makan
+        wdMap = googleMap;
+        PadangOmuda = googleMap;
+        mKontrakan = googleMap;
+        // Add a marker in Sydney and move the camera
+        /*LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        //batas lokasi UPI
+        //urutan harus kiri bawah, kanan atas kotak
+        LatLngBounds UPI = new LatLngBounds(
+                new LatLng(-6.863273, 107.587212),new LatLng(-6.858025, 107.597839));
+
+
+        //marker gedung ilkom
+        LatLng gedungIlkom = new LatLng(-6.860418, 107.589889);
+        mMap.addMarker(new MarkerOptions().position(gedungIlkom).title("Marker di GIK"));
+
+
+        // Set kamera sesuai batas UPI
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(UPI, 0));
+
+        //marker kontrakan
+        Kontrakan = new LatLng(-6.8663673, 107.5918008);
+        mKontrakan.addMarker(new MarkerOptions().position(Kontrakan).title("Kontrakan"));
+
+        //marker warung jadoel
+        Wadoel = new LatLng(-6.8649716,107.5936292);
+        wdMap.addMarker(new MarkerOptions().position(Wadoel).title("Warung Jadoel Cafe"));
+
+        //marker Padang Omuda
+        PadangO = new LatLng(-6.8658617,107.5916296);
+        PadangOmuda.addMarker(new MarkerOptions().position(PadangO).title("Rumah Makan Padang Omuda"));
+
+
+        // Set kamera sesuai batas UPI
+        int width = getResources().getDisplayMetrics().widthPixels;
+        int height = getResources().getDisplayMetrics().heightPixels;
+        int padding = (int) (width * 0.12); // offset dari edges
+
+        //posisi sekarang
+        posSekarang = new LatLng(-6.8663673, 107.5918008);
+
+        mPosSekarang = mMap.addMarker(new MarkerOptions().position(posSekarang).title("PosSekarang"));
+
+        //set kamera sesuai batas di Ilkom
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posSekarang, 17));
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        ambilLokasi();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    //muncul dialog & user memberikan reson (allow/deny), method ini akan dipanggil
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        if (requestCode == MY_PERMISSIONS_REQUEST) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                ambilLokasi();
+            } else {
+                //permssion tidak diberikan, tampilkan pesan
+                AlertDialog ad = new AlertDialog.Builder(this).create();
+                ad.setMessage("Tidak mendapat ijin, tidak dapat mengambil lokasi");
+                ad.show();
+            }
+            return;
+        }
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop(){
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        /*AlertDialog ad = new AlertDialog.Builder(this).create();
+        ad.setMessage("update lokasi");
+        ad.show();*/
+        mPosSekarang.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()), 17));
+
+        //untuk menyamakan lokasi
+        /*if(Kontrakan.equals(new LatLng(location.getLatitude(),location.getLongitude()))){
+            AlertDialog ad = new AlertDialog.Builder(this).create();
+            ad.setMessage("Anda Berada disini");
+            ad.show();
+        }
+        else if(Wadoel.equals(new LatLng(location.getLatitude(),location.getLongitude()))){
+            AlertDialog ad = new AlertDialog.Builder(this).create();
+            ad.setMessage("Anda Berada di Waroeng Jadul");
+            ad.show();
+        }
+        else if(PadangO.equals(new LatLng(location.getLatitude(),location.getLongitude()))){
+            AlertDialog ad = new AlertDialog.Builder(this).create();
+            ad.setMessage("Anda Berada di Rumah Makan Padang Omuda");
+            ad.show();
+        }*/
+        double toKontrakan = distFrom((float)location.getLatitude(),(float)location.getLongitude(),getLat(Kontrakan),getLong(Kontrakan));
+        double toPadangO = distFrom((float)location.getLatitude(),(float)location.getLongitude(),getLat(PadangO),getLong(PadangO));
+        System.out.println("Jarak : " + toKontrakan);
+        if(toKontrakan >= 0 && toKontrakan <=15){
+            System.out.println("Jarak : " + toKontrakan);
+            mPosSekarang.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
+            AlertDialog ad = new AlertDialog.Builder(this).create();
+            ad.setMessage("Anda dekat lokasi kontrakan");
+            ad.show();
+        }
+
+        if(toPadangO >= 0 && toPadangO <=15){
+            System.out.println("Jarak : " + toPadangO);
+            mPosSekarang.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
+            AlertDialog ad = new AlertDialog.Builder(this).create();
+            ad.setMessage("Anda dekat lokasi rumah padang");
+            ad.show();
+        }
+
+
+
+
+    }
+
+    public static float distFrom(float lat1, float lng1, float lat2, float lng2) {
+        double earthRadius = 6371000; //meters
+        double dLat = Math.toRadians(lat2-lat1);
+        double dLng = Math.toRadians(lng2-lng1);
+        double a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+                Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2)) *
+                        Math.sin(dLng/2) * Math.sin(dLng/2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        float dist = (float) (earthRadius * c);
+
+        return dist;
+    }
+
+    public float getLat(LatLng latln) {
+        String s = latln.toString();
+        String[] latLng = s.substring(10, s.length() - 1).split(",");
+        String sLat = latLng[0];
+
+        return Float.parseFloat(sLat);
+    }
+
+    public float getLong(LatLng latln) {
+        String s = latln.toString();
+        String[] latLng = s.substring(10, s.length() - 1).split(",");
+        String sLong = latLng[1];
+
+        return Float.parseFloat(sLong);
+    }
+
+
+
+
+}
